939c171a76285e3e1ed39e3398836310c601464d
tuanit168 <tuanit168@gmail.com>
2021-12-13 23:15:55 +1100

    Replacing deprecated function

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -37,12 +37,18 @@ class _AuthScreenState extends State<AuthScreen> {
         message = err.message!;
       }
 
-      Scaffold.of(ctx).showSnackBar(
+      ScaffoldMessenger.of(ctx).showSnackBar(
         SnackBar(
           content: Text(message),
           backgroundColor: Theme.of(ctx).colorScheme.error,
         ),
       );
+      // Scaffold.of(ctx).showSnackBar(
+      //   SnackBar(
+      //     content: Text(message),
+      //     backgroundColor: Theme.of(ctx).colorScheme.error,
+      //   ),
+      // );
     } catch (err) {
       print(err);
     }
